abae300b13e552f724b9d277361065bbe73138cb
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2025-12-14 19:26:13 +0530

    Update useAppwrite.ts

--- lib/useAppwrite.ts
+++ lib/useAppwrite.ts
@@ -49,7 +49,7 @@ export const useAppwrite = <T, P extends Record<string, string | number>>({
     }
   }, []);
 
-  const refetch = async (newParams: P) => await fetchData(newParams);
+  const refetch = async (newParams?: P) => await fetchData(newParams || params);
 
   return { data, loading, error, refetch };
 };
